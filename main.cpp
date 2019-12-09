19215e329d384b78c41c4f761d36663928e87292
Ulises Peña <ulises.pena@mail.udp.cl>
2019-12-09 18:33:44 -0300

    det fixed

--- main.cpp
+++ main.cpp
@@ -9,11 +9,13 @@ using namespace std;
 class Matrix {
   private:
     vector<vector<float>> matrix;
+    int n;
+    float det;
 
     int get_max_abs(vector<float> v, int j) {
       int index = j;
 
-      for(int i = j; i < v.size(); ++i) {
+      for(int i = j; i < this->n; ++i) {
         if(abs(v[i]) > abs(v[index])) index = i;
       }
 
@@ -21,27 +23,30 @@ class Matrix {
     }
 
     void swap_cols(int u, int v) {
-      for(int i = 0; i < this->matrix.size(); ++i) {
+      for(int i = 0; i < this->n; ++i) {
         float aux = this->matrix[i][u];
         this->matrix[i][u] = this->matrix[i][v];
         this->matrix[i][v] = aux;
       }
+      this->det *= -1.0;
     }
 
     void elim(float m, int u, int v) {
-      for(int i = 0; i < this->matrix.size(); ++i) {
+      for(int i = 0; i < this->n; ++i) {
         this->matrix[i][v] -= m * this->matrix[i][u];
       }
     }
 
     void gauss_elim() {
-      for(int i = 0; i < this->matrix.size(); ++i) {
-        int index_max = this->get_max_abs(matrix[i], i);
-        if(index_max != i) this->swap_cols(index_max, i);
+      for(int i = 0; i < this->n; ++i) {
+        if(this->matrix[i][i] == 0.0) {
+          int index_max = this->get_max_abs(matrix[i], i);
+          if(index_max != i) this->swap_cols(index_max, i);
+        }
 
         float pivot = this->matrix[i][i];
 
-        for(int j = i+1; j < this->matrix.size(); ++j) {
+        for(int j = i+1; j < this->n; ++j) {
           float m = this->matrix[i][j] / pivot;
           this->elim(m, i, j);
         }
@@ -51,11 +56,12 @@ class Matrix {
   public:
     Matrix(int n) {
       this->matrix.resize(n, vector<float>(n,0));
+      this->n = n;
+      this->det = 1.0;
 
       // random number generator
-      default_random_engine rng;
-      // valores entre [-10,10]
-      uniform_real_distribution<float> dist(-2.0,2.0);
+      default_random_engine rng(chrono::system_clock::now().time_since_epoch().count());
+      uniform_real_distribution<float> dist(-1,1);
 
       for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
@@ -67,9 +73,7 @@ class Matrix {
     float determinant() {
       this->gauss_elim();
 
-      float det = 1;
-
-      for(int i = 0; i < this->matrix.size(); ++i) {
+      for(int i = 0; i < this->n; ++i) {
         det *= this->matrix[i][i];
       }
 
@@ -77,8 +81,8 @@ class Matrix {
     }
 
     void print() {
-      for(int i = 0; i < this->matrix.size(); ++i) {
-        for(int j = 0; j < this->matrix[0].size(); j++) {
+      for(int i = 0; i < this->n; ++i) {
+        for(int j = 0; j < this->n; j++) {
           cout << this->matrix[i][j] << ' ';
         }
         cout << endl;
@@ -102,11 +106,9 @@ int main(int argc, char** argv) {
   cout << matrix->determinant() << endl;
 
   auto stop = chrono::high_resolution_clock::now();
-
   auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
 
   cout << endl << duration.count()/1000.0 << " ms" << endl;
-  cout << duration.count()/1000000.0 << " s" << endl;
 
   return 0;
 }
